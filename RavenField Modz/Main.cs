585e936b705416a981995bd40b214d04ddb622ef
Mizar <hmm12dj@hotmail.com>
2023-03-01 03:07:31 -0500

    Updates
    Adding a store initial value function and restore initial value function

--- RavenField Modz/Main.cs
+++ RavenField Modz/Main.cs
@@ -23,8 +23,10 @@ namespace RavenField_Modz
         internal readonly Assembly assembly;
         public readonly string modFolder;
 
-        private static bool _showGui = false;
+        public static bool _showGui = false;
+        public static bool _showGui2 = false;
         public static Rect windowRect = new Rect(0, 0, 150, 250);
+        public static Rect windowRect2 = new Rect(0, 0, 120, 100);
 
         #endregion
 
@@ -41,11 +43,28 @@ namespace RavenField_Modz
             harmony.PatchAll(assembly);
         }
 
+        public void MakeMainWindow(int windowID)
+        {
+            if (GUI.Button(new Rect(10, 20, 130, 20), "LocalPlayer Options"))
+            {
+                _showGui2 = true;
+                if (_showGui2)
+                {
+                    _showGui = false;
+                }
+            }
+            GUI.DragWindow();
+        }
+
         public void OnGUI()
         {
-            if(_showGui)
+            if (_showGui)
             {
-                windowRect = GUI.Window(0, windowRect, Modules.LocalPlayer.Health_Controller.HealthWindow, "Debug Menu");
+                windowRect = GUI.Window(0, windowRect, MakeMainWindow, "Debug Menu");
+            }
+            if (_showGui2)
+            {
+                windowRect2 = GUI.Window(1, windowRect2, Modules.LocalPlayer.Health_Controller.HealthWindow, "LocalPlayer Options");
             }
         }
 
--- RavenField Modz/Modules/LocalPlayer/Health Controller.cs
+++ RavenField Modz/Modules/LocalPlayer/Health Controller.cs
@@ -1,32 +1,54 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace RavenField_Modz.Modules.LocalPlayer
 {
     internal class Health_Controller : MonoBehaviour
     {
         private static bool _isToggled = false;
+        private static float? initialValue;
+
 
         internal static void HealthWindow(int windowID)
         {
-            // Make window draggable
-            GUI.DragWindow(new Rect(0, 0, 10000, 20));
-            // Begins auto aligned vertical contents
-            GUILayout.BeginVertical();
+
+            if (GUI.Button(new Rect(10, 45, 130, 20), "X"))
+            {
+                Main._showGui = true;
+                if (Main._showGui2)
+                {
+                    Main._showGui2 = false;
+                }
+            }
             _isToggled = GUI.Toggle(new Rect(10, 20, 130, 20), _isToggled, "Invulnerability");
             if (_isToggled)
             {
                 Refs.LocalPlayer.isInvulnerable = true;
                 Refs.LocalPlayer.activeWeapon.ammo = 999999;
                 Refs.LocalPlayer.activeWeapon.spareAmmo = 999999;
-                return;
             }
             else if (!_isToggled)
             {
                 Refs.LocalPlayer.isInvulnerable = false;
-                Refs.LocalPlayer.activeWeapon.ammo = 1;
-                Refs.LocalPlayer.activeWeapon.spareAmmo = 10;
+                Refs.LocalPlayer.activeWeapon.ammo = 10;
+                Refs.LocalPlayer.activeWeapon.spareAmmo = 85;
+            }
+            GUI.DragWindow();
+        }
+         internal static void GetInitialValues()
+        {
+            if (Refs.LocalPlayer != null)
+            {
+                initialValue = Refs.LocalPlayer.health;
+                Debug.Log($"[GetInitialValues]: {initialValue}");
                 return;
+            }
+        }
+        internal static void RestoreInitialValues()
+        {
+            if (PlayerPrefs.HasKey("initialValue"))
+            {
+                initialValue = PlayerPrefs.GetFloat("initialValue");
+                Debug.Log($"");
             }
         }
     }
